a568f30da27ca6802d09c910703c6696712497ff
uFirstUser <ufirstuser@LV.amendola46>
2017-12-11 11:33:53 +0100

    Moved some common components to components folder

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -17,11 +17,12 @@ import injectSaga from 'utils/injectSaga';
 import { makeSelectRepos, makeSelectLoading, makeSelectError } from 'containers/App/selectors';
 import H2 from 'components/H2';
 import ReposList from 'components/ReposList';
+import Section from 'components/Section';
+
 import AtPrefix from './AtPrefix';
 import CenteredSection from './CenteredSection';
 import Form from './Form';
 import Input from './Input';
-import Section from './Section';
 import messages from './messages';
 import { loadRepos } from '../App/actions';
 import { changeUsername } from './actions';
